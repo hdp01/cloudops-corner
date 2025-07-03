ef8c76bc2dc5daa9b7dfa27fd2cb98c119364331
Harsh Pashine <pashineharsh01@gmail.com>
2025-07-03 19:20:16 +0530

    updated the ui for the website

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/expertise')
+def expertise():
+    return render_template('expertise.html')
+
+@app.route('/industries')
+def industries():
+    return render_template('industries.html')
+
+@app.route('/case-studies')
+def case_studies():
+    return render_template('case_studies.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
